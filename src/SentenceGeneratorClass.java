f9657ba4c0b2e47105fc9964f7ae203b4dd2957c
Nikhil Gilbert <gilbert.nikhil@gmail.com>
2017-09-02 00:06:23 +0200

    Renamed class and added new instructions

---
+++ src/SentenceGeneratorClass.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Admin on 2017-09-01.
+ */
+
+public class SentenceGeneratorClass{
+
+    private HashMap<String, ArrayList<String>> NounTerminals =new HashMap<>();
+
+    public SentenceGeneratorClass(){
+
+    }
+
+    public String generateNVNsentence(){
+
+    }
+
+    public String generateNVsentence(){
+
+    }
+}
