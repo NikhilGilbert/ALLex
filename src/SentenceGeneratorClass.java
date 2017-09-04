eea6492cd9d2a292497efb2add4b5f6fc0ead190
Nikhil Gilbert <gilbert.nikhil@gmail.com>
2017-09-04 21:06:22 +0200

    Added functionality to load up the VerbChain

--- src/SentenceGeneratorClass.java
+++ src/SentenceGeneratorClass.java
@@ -3,10 +3,6 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
- * Created by Admin on 2017-09-01.
- */
-
 public class SentenceGeneratorClass{
 
     private HashMap<String, ArrayList<String>> NounConsumables = new HashMap<>();
@@ -14,6 +10,7 @@ public class SentenceGeneratorClass{
 
     public SentenceGeneratorClass(){
 
+        // Load nouns into a hashtable
         try{
             FileReader fr = new FileReader("NounChainList.txt");
             BufferedReader br = new BufferedReader(fr);
@@ -32,27 +29,48 @@ public class SentenceGeneratorClass{
                     NounConsumables.put(splitter[0] + " " + splitter[1], approvedVerbs);
                 }
             }
-
         }
         catch (Exception e) {
             e.printStackTrace();
         }
 
+        //Load Verbs into a hashtable
         try{
+            FileReader fr = new FileReader("VerbChain.txt");
+            BufferedReader br = new BufferedReader(fr);
 
+            String currentLine = null;
+
+            while((currentLine = br.readLine()) != null){
+                String [] splitter = currentLine.split(" ");
+                if (splitter.length == 3){
+
+                    ArrayList<String> approvedNouns = new ArrayList<>();
+                    String[] nounSplitter = splitter[2].split(";");
+                    for (int i = 0; i < nounSplitter.length; i++){
+                        approvedNouns.add(nounSplitter[i]);
+                    }
+                    VerbConsumables.put(splitter[0] + " " + splitter[1], approvedNouns);
+                }
+            }
         }
-        catch (Exception e){
-
+        catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
 
     public String generateNVsentence(){
-        //
+        // select random key from the noun list
+        // Add that to the sentence constructor
+
         return null;
     }
 
     public String generateNVNsentence(){
+        return null;
+    }
+
+    public String generateNVVsentence(){
         return null;
     }
 }
